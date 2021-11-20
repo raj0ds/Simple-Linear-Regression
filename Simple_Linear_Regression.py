bcab47c8b72bbdd80caf67c7c96c1156a52d1d2e
RAJEEV ANAND <rajeevanand840@gmail.com>
2021-11-20 19:36:41 +0530

    Update Simple_Linear_Regression.py

--- Simple_Linear_Regression.py
+++ Simple_Linear_Regression.py
@@ -1,1 +1,28 @@
-Importing Needed packages
+# Importing Needed packages
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+%matplotlib inline
+
+# Downloading Data
+# To download the data, we will use !wget to download it from IBM Object Storage.
+!wget -O FuelConsumption.csv https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv
+  
+#  Understanding the Data
+# FuelConsumption.csv:
+# We have downloaded a fuel consumption dataset, FuelConsumption.csv, which contains model-specific fuel consumption ratings and estimated carbon dioxide emissions for new light-duty vehicles for retail sale in Canada. Dataset source
+
+# MODELYEAR e.g. 2014
+# MAKE e.g. Acura
+# MODEL e.g. ILX
+# VEHICLE CLASS e.g. SUV
+# ENGINE SIZE e.g. 4.7
+# CYLINDERS e.g 6
+# TRANSMISSION e.g. A6
+# FUEL CONSUMPTION in CITY(L/100 km) e.g. 9.9
+# FUEL CONSUMPTION in HWY (L/100 km) e.g. 8.9
+# FUEL CONSUMPTION COMB (L/100 km) e.g. 9.2
+# CO2 EMISSIONS (g/km) e.g. 182 --> low --> 0
+
+# <h1> Reading the data in </h1>
